f5bfad2d119f3742dabf1e5126949255a9be19f8
Luv Uprety <luvuprety@Luvs-MacBook-Pro.local>
2023-02-08 01:34:04 +0545

    added requirements file

---
+++ src/app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .models import Bus, Route, Bus_Route
+from .tables import BusTable, BusRouteTable, RouteTable, RouteBusTable
+from django.db.models import Count, QuerySet
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def get_buses(request):
+    return render(request, )
+
+
+
+def get_buses(request):
+    table = BusTable(Bus.objects.annotate(number_of_routes=Count('routes')))
+    context = {
+        'table': table
+    }
+    return render(request, 'table.html', context)
+
+
+
+def get_bus_routes(request, id):
+    bus = Bus.objects.get(pk=id)
+    bus_routes = Bus_Route.objects.filter(bus=bus)
+    table = BusRouteTable(bus_routes)
+    context = {
+        'table' : table
+    }
+    return render(request, 'table.html', context)
+
+
+def get_routes(request):
+    routes = Route.objects.annotate(numbers_of_buses=Count('buses'))
+    table = RouteTable(routes)
+    context = {
+        'table' : table
+    }
+    return render(request, 'table.html', context)
+
+def get_route_buses(request, id):
+    route = Route.objects.get(pk=id)
+    bus_routes = Bus_Route.objects.filter(route=route)
+    table = RouteBusTable(bus_routes)
+    context = {
+        'table' : table
+    }
+    return render(request, 'table.html', context)
